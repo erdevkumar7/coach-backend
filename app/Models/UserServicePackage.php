d201674923f51c6c5a955d711129972845fed7eb
Vivek Gour <votive.vivekgourwp@gmail.com>
2025-07-01 17:55:09 +0530

    Add service package

--- app/Models/UserServicePackage.php
+++ app/Models/UserServicePackage.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class UserServicePackage extends Model
 {
-    //
+        protected $fillable = [
+        'coach_id',
+        'title',
+        'package_status',
+        'short_description',
+        'coaching_category',
+        'description',
+        'focus',
+        'coaching_type',
+        'delivery_mode',
+        'session_count',
+        'session_duration',
+        'target_audience',
+        'price',
+        'currency',
+        'booking_slot',
+        'booking_window',
+        'cancellation_policy',
+        'rescheduling_policy',
+        'media_file',
+        'status',
+    ];
 }

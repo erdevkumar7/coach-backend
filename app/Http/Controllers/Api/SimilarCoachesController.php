88a7346bead4891a96495ab6145f5399503087e3
votiverohit <votivephp.rohit@gmail.com>
2025-08-19 15:06:12 +0530

    add the get coaching paackages

--- app/Http/Controllers/Api/SimilarCoachesController.php
+++ app/Http/Controllers/Api/SimilarCoachesController.php
@@ -6,10 +6,14 @@ use App\Http\Controllers\Controller;
 use App\Models\Professional;
 use App\Models\User;
 use App\Models\CoachingRequest;
+use App\Models\BookingPackages;
 use Illuminate\Http\Request;
 use DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+
 
 class SimilarCoachesController extends Controller
 {
@@ -95,7 +99,7 @@ public function getPendingCoaching(Request $request)
 
     $id = $user->id;
 //    echo $id;die;
-    $perPage = $request->input('per_page', 10);
+    $perPage = $request->per_page ?? 6;
     $page = $request->input('page', 1);
 
     // Determine relationship & filter based on user type
@@ -153,6 +157,101 @@ $results = $coachingRequests->getCollection()->map(function ($req) use ($relatio
 
 public function getCoachingPackages(Request $request)
 {
+    $user = Auth::user(); // Authenticated user
+
+    if (!$user) {
+        return response()->json([
+            'success' => false,
+            'message' => 'User not authenticated.',
+        ], 403);
+    }
+
+    $id = $user->id;
+    $perPage = $request->per_page ?? 6;
+    $page = $request->input('page', 1);
+
+    // echo $id;die;
+
+    // Determine relationship & filter based on user type
+    if ($user->user_type == 2) { // Coach
+        $relation = 'coach';
+        $filterColumn = 'user_id';
+    } else { // Normal User
+        $relation = 'user';
+        $filterColumn = 'coach_id';
+    }
+
+    $bookPackages = BookingPackages::with([
+        $relation . '.country',
+        $relation . '.userProfessional.coachType',
+    ])
+        ->where($filterColumn, $id)
+        ->orderBy('booking_packages.id', 'desc')
+        ->paginate($perPage, ['*'], 'page', $page);
+
+        // print_r($bookPackages);die;
+    $results = $bookPackages->getCollection()->map(function ($req) use ($relation) {
+        $show_relation = $relation;
+
+        // echo $req->$show_relation->id;die;
+        // echo $show_relation;die; 
+       $now = Carbon::now()->startOfDay();
+
+        // Full start datetime
+        $startDateTime = Carbon::parse($req->session_date_start . ' ' . $req->slot_time_start);
+
+        // Full end datetime
+        $endDateTime = Carbon::parse($req->session_date_end . ' ' . $req->slot_time_end);
+        $endDate       = Carbon::parse($req->session_date_end)->endOfDay();
+   
+        if ($now->between($startDateTime, $endDateTime)) {
+            $status = 'in-progress'; 
+        } elseif ($now->lt($startDateTime)) {
+            $status = 'confirmed'; 
+        } 
+
+         $sessionLeft = $now->lte($endDate) 
+                ? $now->diffInDays($endDate) 
+                : 0;
+    //    print_r($sessionLeft);die;
+        return [
+            'id'                => $req->$show_relation->id ?? null,
+            'booking_id'        => $req->id ?? null,
+            'first_name'        => $req->$show_relation->first_name ?? null,
+            'last_name'         => $req->$show_relation->last_name ?? null,
+            'user_type'         => $req->$show_relation->user_type ?? null,
+            'display_name'      => $req->$show_relation->display_name ?? null,
+            'profile_image'     => $req->$show_relation->profile_image
+                ? url('public/uploads/profile_image/' . $req->$show_relation->profile_image)
+                : '',
+            'session_date_start' => $req->session_date_start ?? null,
+            'slot_time_start'    => $req->slot_time_start ?? null,
+            'session_date_end'   => $req->session_date_end ?? null,
+            'slot_time_end'      => $req->slot_time_end ?? null,
+            'country'            => $req->$show_relation->country->country_name ?? null,
+            'status'             => $status ?? null,
+            'session_left'       => $status == 'active'? 'seesion not started yet':round($sessionLeft, 0) - 1 ?? null,
+        ];
+    })->filter(); // remove nulls (completed ones)
+
+    return response()->json([
+        'success' => true,
+        'request_count' => $results->count(),
+        'data' => $results->values(),
+        'pagination' => [
+            'total'        => $bookPackages->total(),
+            'per_page'     => $bookPackages->perPage(),
+            'current_page' => $bookPackages->currentPage(),
+            'last_page'    => $bookPackages->lastPage(),
+            'from'         => $bookPackages->firstItem(),
+            'to'           => $bookPackages->lastItem(),
+        ],
+    ]);
+}
+
+
+public function getCoachingPackages56(Request $request)
+{
     // echo "test";die;
     $user = Auth::user(); // Authenticated user
 
@@ -165,7 +264,7 @@ public function getCoachingPackages(Request $request)
 
     $id = $user->id;
 //    echo $id;die;
-    $perPage = $request->input('per_page', 10);
+    $perPage = $request->per_page ?? 10;
     $page = $request->input('page', 1);
 
     // Determine relationship & filter based on user type
@@ -177,43 +276,57 @@ public function getCoachingPackages(Request $request)
         $filterColumn = 'coach_id';
     }
 
-$coachingRequests = BookingPackages::with([
+$bookPackages = BookingPackages::with([
                         $relation . '.country',  
                         $relation . '.userProfessional.coachType', 
-                        $relation . '.reviews', 
+                        // $relation . '.reviews', 
                     ])->where($filterColumn, $id)
-                    ->orderBy('coaching_request.id', 'desc')
+                    ->orderBy('booking_packages.id', 'desc')
                     ->paginate($perPage, ['*'], 'page', $page);
 
-        print_r($coachingRequests);die;
+        // print_r($bookPackages);die;
         
-$results = $coachingRequests->getCollection()->map(function ($req) use ($relation) {
-    $show_relation = $relation;     
-    $reviews = $req->$show_relation->reviews ?? collect();
-    $avgRating = $reviews->avg('rating'); 
+$results = $bookPackages->getCollection()->map(function ($req) use ($relation) {
+    $show_relation = $relation;    
+
+        $endDate   = Carbon::parse($req->slot_time_end);
+        $today     = Carbon::today();
+
+        if($today <= $endDate){
+            echo 'in-progress';
+        }elseif($today >= $endDate){
+            echo "Active";
+        }
+ 
     return [
         'id'         => $req->$show_relation->id ?? null,
+        'booking_id' => $req->id ?? null,
         'first_name' => $req->$show_relation->first_name ?? null,
         'last_name'  => $req->$show_relation->last_name ?? null,
         'user_type'  => $req->$show_relation->user_type ?? null,
-        'coaching_category'    => $req->coach->userProfessional->coachType->type_name ?? null,
-        'company_name'    => $req->$show_relation->company_name ?? null,
-        'review_coach'    => $avgRating ?? null,
-        'country'    => $req->$show_relation->country->country_name ?? null, // ✅ country name
+        'display_name'  => $req->$show_relation->display_name ?? null,
+        'profile_image' => $req->$show_relation->profile_image
+                    ? url('public/uploads/profile_image/' . $req->$show_relation->profile_image)
+                    : '',
+        'session_date_start'  => $req->session_date_start ?? null,
+        'slot_time_start'  => $req->slot_time_start ?? null,
+        'session_date_end'  => $req->session_date_end ?? null,
+        'slot_time_end'  => $req->slot_time_end ?? null,        
+        'country'    => $req->$show_relation->country->country_name ?? null, 
     ];
 });
 //  echo 'test';die;
     return response()->json([
         'success' => true,
-        'request_count' => $coachingRequests->total(),
+        'request_count' => $bookPackages->total(),
         'data' => $results,
         'pagination' => [
-            'total'        => $coachingRequests->total(),
-            'per_page'     => $coachingRequests->perPage(),
-            'current_page' => $coachingRequests->currentPage(),
-            'last_page'    => $coachingRequests->lastPage(),
-            'from'         => $coachingRequests->firstItem(),
-            'to'           => $coachingRequests->lastItem(),
+            'total'        => $bookPackages->total(),
+            'per_page'     => $bookPackages->perPage(),
+            'current_page' => $bookPackages->currentPage(),
+            'last_page'    => $bookPackages->lastPage(),
+            'from'         => $bookPackages->firstItem(),
+            'to'           => $bookPackages->lastItem(),
         ],
     ]);
 }
--- app/Models/BookingPackages.php
+++ app/Models/BookingPackages.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class BookingPackages extends Model
 {
     protected $table = 'booking_packages';
+
+        public function coach()
+    {
+        return $this->belongsTo(User::class, 'coach_id');
+    }
+
+        public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }

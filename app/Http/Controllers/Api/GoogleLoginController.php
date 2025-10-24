d8fcbfa8fe6a49a0c242afcda60115b7fe8c6899
erdevkumar7 <erdevkumar7@gmail.com>
2025-10-24 19:04:44 +0530

    chnages for google login Controller3

--- app/Http/Controllers/Api/GoogleLoginController.php
+++ app/Http/Controllers/Api/GoogleLoginController.php
@@ -16,44 +16,44 @@ class GoogleLoginController extends Controller
         return Socialite::driver('google')->stateless()->redirect();
     }
 
-    // public function callback(Request $request)
-    // {
-    //     try {
-    //         $googleUser = Socialite::driver('google')->stateless()->user();
+    public function callback(Request $request)
+    {
+        try {
+            $googleUser = Socialite::driver('google')->stateless()->user();
 
-    //         $userType = session('user_type', 'user');
+            $userType = session('user_type', 'user');
 
-    //         $firstName = $googleUser->user['given_name'] ?? '';
-    //         $lastName  = $googleUser->user['family_name'] ?? '';
+            $firstName = $googleUser->user['given_name'] ?? '';
+            $lastName  = $googleUser->user['family_name'] ?? '';
 
-    //         $user = User::updateOrCreate(
-    //             ['email' => $googleUser->getEmail()],
-    //             [
-    //                 'first_name' => $firstName,
-    //                 'last_name'  => $lastName,
-    //                 'google_id'  => $googleUser->getId(),
-    //                 'avatar'     => $googleUser->getAvatar(),
-    //                 'user_type'  => ($userType === 'coach') ? 3 : 2,
-    //                 'user_status'=> 1,
-    //                 'email_verified' => 1,
-    //                 'is_social'  => 1,
-    //                 'is_deleted' => 0,
-    //                 'is_verified' => 1,
-    //                 'is_corporate' => 1,
-    //                 'is_active' => 1,
-    //             ]
-    //         );
+            $user = User::updateOrCreate(
+                ['email' => $googleUser->getEmail()],
+                [
+                    'first_name' => $firstName,
+                    'last_name'  => $lastName,
+                    'google_id'  => $googleUser->getId(),
+                    'avatar'     => $googleUser->getAvatar(),
+                    'user_type'  => ($userType === 'coach') ? 3 : 2,
+                    'user_status'=> 1,
+                    'email_verified' => 1,
+                    'is_social'  => 1,
+                    'is_deleted' => 0,
+                    'is_verified' => 1,
+                    'is_corporate' => 1,
+                    'is_active' => 1,
+                ]
+            );
 
-    //         $token = JWTAuth::fromUser($user);
+            $token = JWTAuth::fromUser($user);
 
-    //         $redirectUrl = env('FRONTEND_URL') . '/login?' . http_build_query([
-    //             'user_type' => $userType,
-    //             'token'     => $token,
-    //         ]);
+            $redirectUrl = env('FRONTEND_URL') . '/login?' . http_build_query([
+                'user_type' => $userType,
+                'token'     => $token,
+            ]);
 
-    //         return redirect()->away($redirectUrl);
-    //     } catch (\Exception $e) {
-    //         return response()->json(['error' => $e->getMessage()], 500);
-    //     }
-    // }
+            return redirect()->away($redirectUrl);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
 }
